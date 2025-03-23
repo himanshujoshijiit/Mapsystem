9ee2dbd7b5a47f983c8abe2b72463d8de41dd55b
himanshujoshi <himanshujiit2018@gmail.com>
2025-03-23 11:52:28 +0530

    intial code

---
+++ src/main/java/com/hjandcompany/mapsystem/MapsystemApplication.java
@@ -0,0 +1,13 @@
+package com.hjandcompany.mapsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hjandcompany/mapsystem/MapsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjandcompany.mapsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
